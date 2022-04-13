3e15274bfc1a5d883f545799dadf938b52b50b11
Ftorresnun <torres.nufed21@zaragoza.salesianos.edu>
2022-04-13 11:57:44 +0200

    Mapa v.0.1

---
+++ Assets/Mapa/Scripts/Levels.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+    private int nivel;
+    public int Nivel { get => nivel; set => nivel = value; }
+
+    public int pruebaCambio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moverEscena();
+    }
+
+    public void moverEscena()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+}
