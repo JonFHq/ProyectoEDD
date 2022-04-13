95b71f16f7546083cf383bbaf6cbd9d8cc4d826f
Ftorresnun <torres.nufed21@zaragoza.salesianos.edu>
2022-04-13 22:21:55 +0200

    Mapa v.0.2

---
+++ Assets/Mapa/Scripts/Enemigo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Morir();
+    }
+
+    public void Morir()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //destruye el enemigo
+            Destroy(enemy);
+            Debug.Log("Se muere el enemigo");
+        }
+    }
+}
--- Assets/Mapa/Scripts/Levels.cs
+++ Assets/Mapa/Scripts/Levels.cs
@@ -5,29 +5,68 @@ using UnityEngine;
 
 public class Levels : MonoBehaviour
 {
-    private int nivel;
-    public int Nivel { get => nivel; set => nivel = value; }
+    public int nivel;
+    //cambiador es el objeto que tiene el script
+    public static GameObject cambiador;
+    public Enemigo enemy;
 
-    public int pruebaCambio;
+    private void Awake()
+    {
+        if (cambiador != null)//si hay un cambiador en la escena me destruyo
+        {
+            Destroy(this.gameObject);//detruyo el gameObject que este "duplicado"
+            return;
+        }
+        //relleno la variable conmigo mismo
+        cambiador = this.gameObject;
+        DontDestroyOnLoad(cambiador);//no se destruya en cambios de escena
+    }
 
-    // Start is called before the first frame update
+    private void OnEnable()//se llama cuando carga
+    {
+        SceneManager.sceneLoaded += OnLoadScene;//llama a la funcion cuando cargas la escena, += te subscribes al evento sceneLoaded
+    }
+
+    public void OnLoadScene(Scene scene, LoadSceneMode mode)
+    {
+        Enemigo[] enemigos = FindObjectsOfType<Enemigo>();//busca en la escena los gameObjects que tengan el script enemigo
+        
+    }
+
     void Start()
     {
-
+        nivel = 1;
+        Debug.Log("el nivel es: " + nivel);
     }
 
     // Update is called once per frame
     void Update()
     {
-        moverEscena();
+        MoverEscena();
     }
 
-    public void moverEscena()
+    public void MoverEscena()
     {
-        if (Input.GetKeyDown("f"))
+        Scene currentScence = SceneManager.GetActiveScene();
+        if (nivel >= 1)
         {
-            SceneManager.LoadScene(1);
+            if (enemy == null)//si no hay enemigo 
+            {
+                Debug.Log("Se ha muerto el enemigo, me cambio de escena");
+                int escenas = Random.Range(0, 3);//el id de las escenas
+                if (currentScence == SceneManager.GetSceneByBuildIndex(escenas))//mira si la escena que estoy va a ser la misma que la que va a cargar
+                {
+                    //llamo a la misma funcion hasta que cargue una escena distinta
+                    MoverEscena();
+                    return;
+                }
+                SceneManager.LoadScene(escenas);//hace random de las escenas 0,1,2
+                nivel++;
+            }
         }
-
+    }
+    private void OnDisable()//se llama cuando el componente(script) se desactiva
+    {
+        SceneManager.sceneLoaded -= OnLoadScene;
     }
 }
