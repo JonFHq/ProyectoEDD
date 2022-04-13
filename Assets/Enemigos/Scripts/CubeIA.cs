77720ddc8206e73aa7d5edd18146f97981ebe44f
SaijaxS <javierburillo2@gmail.com>
2022-04-13 22:39:02 +0200

    Enemigos v.0.3

--- Assets/Enemigos/Scripts/CubeIA.cs
+++ Assets/Enemigos/Scripts/CubeIA.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CubeIA : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public float Distance;
+    public GameObject Player;
+    public bool IsPlayerInSight;
+ public Transform target;
+    public Transform mytransform;
+    public GameObject enemy;
+    public float elife = 100f;
+     GameObject ebullet;
+
+public NavMeshAgent agent;
+
+
+public void Start(){
+ ebullet = Resources.Load("bullets") as GameObject;
+}
+public void FixedUpdate(){
+    Distance = Vector3.Distance(Player.transform.position, this.transform.position);
+    if(Distance <= 100000){
+        IsPlayerInSight = true;
+    }
+    else{
+        IsPlayerInSight = false;
+    }
+    if (IsPlayerInSight){
+        agent.isStopped = false;
+        agent.SetDestination(Player.transform.position);
+    }
+    if (!IsPlayerInSight){
+        agent.isStopped = true;
+    }
+    transform.LookAt(target);
+
+    
+    transform.Translate(Vector3.forward * 5 * Time.fixedDeltaTime);
+    GameObject bullets = Instantiate(ebullet) as GameObject;
+    bullets.transform.position= transform.position * 1;
+    Rigidbody rb = bullets.GetComponent<Rigidbody>();
+    rb.velocity = transform.forward * 20;
+    Destroy(bullets, 9f);
+    if(elife <= 0){
+        Destroy(enemy);
     }
 }
+    
+}
+
