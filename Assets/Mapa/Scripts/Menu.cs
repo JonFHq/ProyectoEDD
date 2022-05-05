949d33854a5142f21511b3b6354c2638ebe2b0df
Ftorresnun <torres.nufed21@zaragoza.salesianos.edu>
2022-05-05 22:29:50 +0200

    Mapa v.1.1

--- Assets/Mapa/Scripts/Menu.cs
+++ Assets/Mapa/Scripts/Menu.cs
@@ -20,12 +20,11 @@ public class Menu : MonoBehaviour
     public void ButtonChange()
     {
         Scene currentScence = SceneManager.GetActiveScene();
-        int escenas = Random.Range(0, 3);//el id de las escenas
-        if (currentScence == SceneManager.GetSceneByBuildIndex(escenas))//mira si la escena que estoy va a ser la misma que la que va a cargar
+        int escenas = Random.Range(0, 3);
+        if (currentScence == SceneManager.GetSceneByBuildIndex(escenas))
         {
-            //llamo a la misma funcion hasta que cargue una escena distinta
             return;
         }
-        SceneManager.LoadScene(escenas);//hace random de las escenas 0,1,2
+        SceneManager.LoadScene(escenas);
     }
 }
