932941aa9265d3ff2a90e5ab37bc5583d45fb690
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2022-05-24 14:07:56 +0200

    Rigibody en la bola

--- Assets/Jugador/Scripts/Movimiento.cs
+++ Assets/Jugador/Scripts/Movimiento.cs
@@ -7,9 +7,9 @@ public class Movimiento : MonoBehaviour
     public float velocidad = 1f;
     public float tiempoRotacion = 0.1f;
     float velocidadRotacion;
+    public Transform camera;
     public float velocidadSalto;
     public CharacterController controlador;
-    Vector3 mirar;
     //int dobleSalto = 0;
     // Start is called before the first frame update
     void Start()
@@ -19,30 +19,20 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float direccionHorizontal = Input.GetAxisRaw("Horizontal");
         float direccionVertical = Input.GetAxisRaw("Vertical");
         Vector3 direccion = new Vector3(direccionHorizontal, 0, direccionVertical);
         if(direccion != Vector3.zero)
         {
-            float anguloObjetivo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + mirar.y;
+            float anguloObjetivo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
             float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloObjetivo, ref velocidadRotacion, tiempoRotacion);
             transform.rotation = Quaternion.Euler(0, angulo, 0);
             Vector3 movimientoDireccion = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
-            controlador.Move(movimientoDireccion * velocidad * Time.deltaTime);
+            controlador.Move(movimientoDireccion.normalized * velocidad * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            mirar.y -= 90 * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(0, mirar.y, 0);
-
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            mirar.y += 90 * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(0, mirar.y, 0);
         }
     }
 }
