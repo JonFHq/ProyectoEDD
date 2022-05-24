8d1e42b5cfa33116687d8dd2f8d1e485d0ceec9e
SaijaxS <javierburillo2@gmail.com>
2022-05-24 14:27:34 +0200

    Cambios sobre enemigos, correcto funcionamiento IA

--- Assets/Enemigos/Scripts/CubeIA.cs
+++ Assets/Enemigos/Scripts/CubeIA.cs
@@ -15,55 +15,71 @@ public class CubeIA : MonoBehaviour
 
     public GameObject bullet;
     public Transform shootPoint;
-    public float shootSpeed = 1;
+    public float shootSpeed = 100;
     public float timeBetweenShots = 3;
-    float originalTime=5;
+    float originalTime=3;
 
 
     public void Start()
     {
 
-        shootSpeed = 1;
+        shootSpeed = 50;
         timeBetweenShots = 3;
         
     }
     public void FixedUpdate()
     {
         Distance = Vector3.Distance(Player.transform.position, this.transform.position);
-        if (Distance > 10 && Distance <= 1000)
+        if (Distance <= 25)
         {
             IsPlayerInSight = true;
+            agent.isStopped=false;
+            agent.SetDestination(Player.transform.position);
         }
         else
         {
             IsPlayerInSight = false;
+
         }
-        if (IsPlayerInSight)
+        if (IsPlayerInSight && Distance<=15)
         {
-            agent.isStopped = false;
-            agent.SetDestination(Player.transform.position);
-        }
-       /* if (!IsPlayerInSight)
-        {
-            agent.isStopped = true;
             timeBetweenShots -= Time.deltaTime;
             if (timeBetweenShots <= 0)
             {
                 ShootPlayer();
                 timeBetweenShots = originalTime;
             }
-        }*/
+           // agent.isStopped = false;
+           // agent.SetDestination(Player.transform.position);
+        }
+        if (!IsPlayerInSight)
+        {
+            agent.isStopped = true;
+            
+        }
         transform.LookAt(target);
     }
 
-   /* private void ShootPlayer()
+    private void ShootPlayer()
     {
         GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
         Rigidbody rig = currentBullet.GetComponentInChildren<Rigidbody>();
 
         rig.AddForce(transform.forward * shootSpeed, ForceMode.VelocityChange);
+        //si el objeto currentBullet llega a la posicion en el eje Y desaparece.
+
     }
-*/
+    //if a bullet hits the player, the bullet dissapears
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+ 
+
+
 
 }
 
