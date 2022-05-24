bbde2439d69dc074504e26a3f5d45c6c220516ec
JonFHq <fernandez.hujon21@zaragoza.salesianos.edu>
2022-05-24 16:11:48 +0200

    Movement fixed y encapsulado

--- Assets/Jugador/Scripts/Camara.cs
+++ Assets/Jugador/Scripts/Camara.cs
@@ -4,31 +4,10 @@ using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
-    public GameObject left_camera;
-    public GameObject right_camera;
-    bool movimientoCamara;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        movimientoCamara = false;
-    }
-
-    // Update is called once per frame
+    public GameObject target;
+    public float xOffset, yOffset, zOffset;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            if (movimientoCamara)
-            {
-                transform.position = right_camera.transform.position;
-                movimientoCamara = !movimientoCamara;
-            }
-            else
-            {
-                transform.position = left_camera.transform.position;
-                movimientoCamara = !movimientoCamara;
-            }
-        }
+        
     }
 }
--- Assets/Jugador/Scripts/Movimiento.cs
+++ Assets/Jugador/Scripts/Movimiento.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    public float velocidad = 1f;
-    public float tiempoRotacion = 0.1f;
-    float velocidadRotacion;
-    public Transform camera;
-    public float velocidadSalto;
-    public CharacterController controlador;
+    private float xInput, zInput;
+    public Rigidbody rb;
+    public float speed = 5f;
+
     //int dobleSalto = 0;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        float direccionHorizontal = Input.GetAxisRaw("Horizontal");
-        float direccionVertical = Input.GetAxisRaw("Vertical");
-        Vector3 direccion = new Vector3(direccionHorizontal, 0, direccionVertical);
-        if(direccion != Vector3.zero)
-        {
-            float anguloObjetivo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloObjetivo, ref velocidadRotacion, tiempoRotacion);
-            transform.rotation = Quaternion.Euler(0, angulo, 0);
-            Vector3 movimientoDireccion = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
-            controlador.Move(movimientoDireccion.normalized * velocidad * Time.deltaTime);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-        }
+        GetInputs();
+    }
+
+    void FixedUpdate() {
+        Movement();
+    }
+
+    private void GetInputs()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+    }
+
+    private void Movement()
+    {
+        Vector3 direction = new Vector3(xInput, 0, zInput);
+        rb.AddForce(direction * speed);
     }
 }
